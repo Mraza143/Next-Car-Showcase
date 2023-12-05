b41da878a16d6b588c035cf68fd358d3fe5b6472
Raza <mraza6601@gmail.com>
2023-12-05 20:31:26 +0500

    api added

--- utils/index.ts
+++ utils/index.ts
@@ -45,7 +45,7 @@ export async function fetchCars(filters: FilterProps) {
 
   // Set the required headers for the API request
   const headers: HeadersInit = {
-    "X-RapidAPI-Key":  "",
+    "X-RapidAPI-Key":  process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
 
